230c6c52b4a750e7ee46447189e5844a728a01bb
Kamil <kamilkasjan@poczta.fm>
2020-12-05 22:04:52 +0100

    add failed stiching detection

--- detect.py
+++ detect.py
@@ -10,12 +10,13 @@ Created on Mon Nov  2 07:55:10 2020
 import cv2 as cv
 import numpy as np
 from copy import copy
+from matplotlib import pyplot as plt
 
 kernel = np.array([[0,-1,0,], [-1,4,-1],[0,-1,0]])
 dilation_kernel = np.ones((5,5), np.uint8)
 erode_kernel = np.ones((9,9), np.uint8)
 
-low_thres = 125
+low_thres = 126
 up_thres = 180
 dest = None
 edges = None
@@ -48,26 +49,54 @@ def on_change_upper(value):
 def find_black_rect(src, edges):
     gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
     sum = 0
-    in_edge = False
+    in_edge = None
     for i in range (0, gray.shape[0]):
+        in_edge = False
         for j in range(0, gray.shape[1]):
-            if edges[i,j] == 255:
-                in_edge = True
+            if (edges[i,j] == 255):
+                if in_edge == False:
+                    in_edge = True
+                else:
+                    in_edge = False
             if in_edge and gray[i,j] == 0:
                 sum = sum+1          
     if sum <5:
-        print("Image correct")
+        return True 
     else:
-        print("Image corrupted")
- 
+        return False
     
-    
-    
+def check_correct_stich(src, edges):
+    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    sum = 0;
+    artifacts_detected = 0
+    stiching_edge_threshold = 120
+    margin_y = 300
+    critical_sum = margin_y*0.1
+    in_edge = False
+    for i in range(0, gray.shape[0]-margin_y):
+        for j in range(0, gray.shape[1]-1):
+            if (edges[i,j] == 255):
+                if in_edge == False:
+                    in_edge = True
+                else:
+                    in_edge = False
+            if in_edge and (abs(int(gray[i,j])-int(gray[i,j+1]))>stiching_edge_threshold):
+                sum = 0
+                for x in range(i, i+margin_y):
+                    if abs(int(gray[x,j])-int(gray[x,j+1]))>stiching_edge_threshold:
+                        sum = sum + 1
+                        if sum >= critical_sum:
+                            artifacts_detected = artifacts_detected + 1
+        in_edge = False 
+    if artifacts_detected > 0:
+        return False
+    else:
+        return True
 
 scale = 10
 hsv = None
 
-img = cv.imread('sample/defect/map_4.jpg')
+img = cv.imread('sample/defect/map_2.jpg')
 width = int(img.shape[1]*scale/100)
 height = int(img.shape[0]*scale/100)
 dim = (width,height)
@@ -79,9 +108,16 @@ cv.createTrackbar("Lower threshold", "Edges", low_thres, 255, on_change_lower)
 cv.createTrackbar("Upper threshold", "Edges", up_thres, 255, on_change_upper)
 color, saturation, value = cv.split(hsv)
 edges, detect = thres_filter(low_thres, up_thres)
-find_black_rect(img_resized, edges)
+if find_black_rect(img_resized, edges):
+        print("No rectangle artifacts detected")
+else:
+        print("Image corrupted")
+if check_correct_stich(img_resized, edges):
+    print("Image is stiched correctly")
+else:
+    print("Image stiching failed")
 # cv.imshow("img",img_resized )
-while True:
-    if cv.waitKey(100)==27:
-        break
+# while True:
+#     if cv.waitKey(100)==27:
+#         break
 cv.destroyAllWindows()
